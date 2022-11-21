55a95c44a830ebc21a76f6696dd2ea8f24023990
Alvimar Felipe <alvimarfelipe3@gmail.com>
2022-11-21 00:50:32 -0300

    filtro grafico1

---
+++ src/models/dadosfic.ts
@@ -0,0 +1,50 @@
+
+type data1 = {
+    id: string,
+    mes: string
+};
+
+export const datames: data1[] = [
+    {id:'01' ,mes:'Janeiro'},
+    {id:'02' ,mes:'Fevereiro'},
+    {id:'03' ,mes:'Março'},
+    {id:'04' ,mes:'Abril'},
+    {id:'05' ,mes:'Maio'},
+    {id:'06' ,mes:'Junho'},
+    {id:'07' ,mes:'Julho'},
+    {id:'08' ,mes:'Agosto'},
+    {id:'09' ,mes:'Setembro'},
+    {id:'10' ,mes:'Outubro'},
+    {id:'11' ,mes:'Novembro'},
+    {id:'12' ,mes:'Dezembro'},
+
+];
+
+type data2 = {
+    di: number,
+    ano: number
+};
+
+export const dataano: data2[] = [
+    {di:1 ,ano:2018},
+    {di:2 ,ano:2019},
+    {di:3 ,ano:2020},
+
+];
+
+type data3 ={
+    nomeFuncionario:string
+};
+
+export const nomeFun: data3[] = [ 
+    {nomeFuncionario:'José'},
+    {nomeFuncionario:'Maria'},
+    {nomeFuncionario:'Felipe'},
+    {nomeFuncionario:'Tadeu'},
+    {nomeFuncionario:'Gabriela'},
+    {nomeFuncionario:'Guilherme'},
+    {nomeFuncionario:'Monica'},
+    {nomeFuncionario:'Casacao'},
+    {nomeFuncionario:'Cebolinha'},
+    {nomeFuncionario:'Magali'},
+]
